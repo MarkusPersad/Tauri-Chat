8a8f134f8311a2643ac62c9a0dbd5c13b488abee
MarkusPersad <msp060308@gmail.com>
2025-02-04 15:27:06 +0800

    使用系统默认通知图标

--- src/utils/notification.js
+++ src/utils/notification.js
@@ -1,12 +1,11 @@
-import { resolveResource } from "@tauri-apps/api/path"
 import { isPermissionGranted, requestPermission, sendNotification } from "@tauri-apps/plugin-notification"
 
-const ICONS = async () => ({
-    info: await resolveResource('icons/info.png'),
-    warning: await resolveResource('icons/warning.png'),
-    success: await resolveResource('icons/color-success.png'),
-    error: await resolveResource('icons/color-error.png')
-})
+const ICONS = {
+    info: "dialog-information",
+    warning: "dialog-warning",
+    success: "dialog-ok",
+    error: "dialog-error"
+}
 export const SendNotification = async (title, content, type) => {
     let permissionGranted = await isPermissionGranted()
     if (!permissionGranted) {
@@ -17,7 +16,7 @@ export const SendNotification = async (title, content, type) => {
         sendNotification({
             title: title,
             body: content,
-            icon: await ICONS[type]
+            icon: ICONS[type]
         })
     }
 }
