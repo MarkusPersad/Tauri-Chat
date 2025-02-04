eccc142ac4db05f1e37a5af76430a754a13b7751
MarkusPersad <msp060308@gmail.com>
2025-02-04 22:22:42 +0800

    添加路由导航

--- src/components/Cheader.vue
+++ src/components/Cheader.vue
@@ -1,5 +1,22 @@
 <template>
     <NavBar data-tauri-drag-region>
+        <template #start>
+            <div class="dropdown dropdown-right">
+                <div tabindex="0" role="button" class="btn btn-ghost btn-circle">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24"
+                        stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="M4 6h16M4 12h16M4 18h7" />
+                    </svg>
+                    <ul tabindex="0"
+                        class="menu menu-sm dropdown-content rounded-box bg-base-100 z-1 mt-3 w-52 p-2 shadow">
+                        <li><a>Homepage</a></li>
+                        <li><a>Portfolio</a></li>
+                        <li><a>About</a></li>
+                    </ul>
+                </div>
+            </div>
+        </template>
         <template #end>
             <div class="iconfont iconfont-jianhao hover:text-black" @click="minimizeWindow"></div>
             <div class="hover:text-black iconfont" :class="{ 'iconfont-dinging': Ding, 'iconfont-ding': !Ding }"
