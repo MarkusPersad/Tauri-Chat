fa77b122cf8321de476322c3d66601efbff464b2
MarkusPersad <msp060308@gmail.com>
2025-02-11 01:32:30 +0800

    更新依赖,使用if let代替match,提高代码简洁性

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -10,12 +10,11 @@ pub fn run() {
         .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_http::init())
         .plugin(tauri_plugin_notification::init())
-        .on_window_event(|window, event| match event {
-            tauri::WindowEvent::CloseRequested { api, .. } => {
+        .on_window_event(|window, event| {
+            if let tauri::WindowEvent::CloseRequested { api, .. } = event {
                 window.hide().unwrap();
                 api.prevent_close();
             }
-            _ => {}
         })
         .invoke_handler(tauri::generate_handler![])
         .run(tauri::generate_context!())
